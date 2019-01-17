985f318c2a39d6f2e51f3d93d407f829ba7435a7
adarshrise <adarshrise20@gmail.com>
2019-01-18 03:17:30 +0530

    deleted wrong 1..py

--- 1..py
+++
@@ -1,6 +0,0 @@
-print("Q. From a given list, find the second highest value from the list.\nInput: [6, 5, 2, 1, 6, 4]\nOutput: 5\n\n")
-l= [6,4,8,4,2,6]
-print("Highest value from the list", max(l))
-
-
-
